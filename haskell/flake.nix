dfd0db00e2bbd37f06511ff34e9e6bd5d9275ca1
Gavin Downard <gavin.downard@runbox.com>
2021-11-06 20:16:55 -0700

    Change haskell template to use haskell.nix

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -1,38 +1,39 @@
-# see https://github.com/serokell/templates/blob/master/haskell-cabal2nix/flake.nix
 {
-  description = "My haskell application";
-
-  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  description = "A basic flake for a haskell project";
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        haskellPackages = pkgs.haskellPackages;
-
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
-
-        packageName = throw "put your package name here!";
-      in rec {
-        packages.${packageName} = haskellPackages.callCabal2nix packageName self
-          rec {
-            # Dependency overrides go here (use to remove cabal version constraints)
-            #
-            # gi-gtk-declarative =
-            #   jailbreakUnbreak haskellPackages.gi-gtk-declarative;
-          };
-
-        defaultPackage = self.packages.${system}.${packageName};
-
-        devShell = haskellPackages.shellFor {
-          packages = ps: [ packages.${packageName} ];
-          buildInputs = with haskellPackages; [
-            haskell-language-server
-            hoogle
-          ];
-          withHoogle = false;
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            # This overlay adds our project to pkgs
+            helloProject = final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc8107";
+              # This is used by `nix develop .` to open a shell for use with
+              # `cabal`, `hlint` and `haskell-language-server`
+              shell.tools = {
+                # cabal = { };
+                # hlint = { };
+                # haskell-language-server = { };
+              };
+              # Non-Haskell shell tools go here
+              shell.buildInputs = with pkgs;
+                [ # nixpkgs-fmt
+                ];
+            };
+          })
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
         };
+        flake = pkgs.helloProject.flake { };
+      in flake // {
+        # Built by `nix build .`
+        defaultPackage = flake.packages."hello:exe:hello";
       });
 }
